442aea14e75e179e0f8b0f4ff4ed265e7bf8a966
Chris Twaddell <ct9233@gmail.com>
2020-06-18 22:47:33 -0700

    Reservation Form Scaffolding

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem,
-	Jumbotron, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+	Jumbotron, Button, Modal, ModalHeader, ModalBody, Label, Col, Row } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import { Control, Form, Errors } from 'react-redux-form';
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 class Header extends Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
+		this.state = {
 			isNavOpen: false,
-			isModalOpen: false
+			isModalOpen: false,
+			firstName: '',
+			lastName: '',
+			phoneNum: '',
+			email: '',
+			requests: '',
+			touched: {
+				firstName: false,
+				lastName: false,
+				phoneNum: false,
+				email: false,
+			},
 		};
 		this.toggleNav = this.toggleNav.bind(this);
 		this.toggleModal = this.toggleModal.bind(this);
-    }
-
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	
-	toggleModal() {
+
+	toggleNav() {
 		this.setState({
-			isModalOpen: !this.state.isModalOpen
+			isNavOpen: !this.state.isNavOpen,
 		});
 	}
 
-    render() {
-        return (
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	}
+
+	handleSubmit(values) {
+		this.props.resetReservationForm();
+		this.props.postReservation(values);
+	}
+
+	render() {
+		return (
 			<React.Fragment>
 				<Jumbotron fluid>
 					<div className='container'>
@@ -62,7 +84,7 @@ class Header extends Component {
 								<NavLink
 									className='nav-link'
 									to='/home'
-									activeStyle={{ color: "#488cf3" }}>
+									activeStyle={{ color: '#488cf3' }}>
 									<i className='fa fa-home fa-lg' /> Home
 								</NavLink>
 							</NavItem>
@@ -70,7 +92,7 @@ class Header extends Component {
 								<NavLink
 									className='nav-link'
 									to='/menu'
-									activeStyle={{ color: "#488cf3" }}>
+									activeStyle={{ color: '#488cf3' }}>
 									<i className='fa fa-list-alt fa-lg' /> Menu
 								</NavLink>
 							</NavItem>
@@ -78,7 +100,7 @@ class Header extends Component {
 								<NavLink
 									className='nav-link'
 									to='/story'
-									activeStyle={{ color: "#488cf3" }}>
+									activeStyle={{ color: '#488cf3' }}>
 									<i className='fa fa-info fa-lg' /> Story
 								</NavLink>
 							</NavItem>
@@ -92,25 +114,174 @@ class Header extends Component {
 							</NavItem>
 						</Nav>
 						<span className='ml-auto'>
-							<Button 
+							<Button
 								onClick={this.toggleModal}
-								style={{color: 'black', backgroundColor: '#488cf3', borderColor: 'darkslategrey'}}>
+								style={{
+									color: 'black',
+									backgroundColor: '#488cf3',
+									borderColor: 'darkslategrey',
+								}}>
 								Make Reservation
 							</Button>
 						</span>
 					</Collapse>
 				</Navbar>
-				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} centered>
-					<ModalHeader toggle={this.toggleModal}>
-						Reservation
-					</ModalHeader>
+				<Modal
+					isOpen={this.state.isModalOpen}
+					toggle={this.toggleModal}
+					centered>
+					<ModalHeader toggle={this.toggleModal}>Reservation</ModalHeader>
 					<ModalBody>
-
+						<Form
+							model='reservationForm'
+							onSubmit={(values) => this.handleSubmit(values)}>
+							<Row className='form-group'>
+								<Label htmlFor='firstName' md={2}>
+									First Name
+								</Label>
+								<Col md={10}>
+									<Control.text
+										model='.firstName'
+										id='firstName'
+										name='firstName'
+										placeholder='First Name'
+										className='form-control'
+										validators={{
+											required,
+											minLength: minLength(2),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className='text-danger'
+										model='.firstName'
+										show='touched'
+										component='div'
+										messages={{
+											required: 'Required',
+											minLength: 'Must be at least 2 characters',
+											maxLength: 'Must be 15 characters or less',
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Label htmlFor='lastName' md={2}>
+									Last Name
+								</Label>
+								<Col md={10}>
+									<Control.text
+										model='.lastName'
+										id='lastName'
+										name='lastName'
+										placeholder='Last Name'
+										className='form-control'
+										validators={{
+											required,
+											minLength: minLength(2),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className='text-danger'
+										model='.lastName'
+										show='touched'
+										component='div'
+										messages={{
+											required: 'Required',
+											minLength: 'Must be at least 2 characters',
+											maxLength: 'Must be 15 characters or less',
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Label htmlFor='phoneNum' md={2}>
+									Phone
+								</Label>
+								<Col md={10}>
+									<Control.text
+										model='.phoneNum'
+										id='phoneNum'
+										name='phoneNum'
+										placeholder='Phone number'
+										className='form-control'
+										validators={{
+											required,
+											minLength: minLength(10),
+											maxLength: maxLength(15),
+											isNumber,
+										}}
+									/>
+									<Errors
+										className='text-danger'
+										model='.phoneNum'
+										show='touched'
+										component='div'
+										messages={{
+											required: 'Required',
+											minLength: 'Must be at least 10 numbers',
+											maxLength: 'Must be 15 numbers or less',
+											isNumber: 'Must be a number',
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Label htmlFor='email' md={2}>
+									Email
+								</Label>
+								<Col md={10}>
+									<Control.text
+										model='.email'
+										id='email'
+										name='email'
+										placeholder='Email'
+										className='form-control'
+										validators={{
+											required,
+											validEmail,
+										}}
+									/>
+									<Errors
+										className='text-danger'
+										model='.email'
+										show='touched'
+										component='div'
+										messages={{
+											required: 'Required',
+											validEmail: 'Invalid email address',
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Label htmlFor='requests' md={2}>
+									Special Requests
+								</Label>
+								<Col md={10}>
+									<Control.textarea
+										model='.requests'
+										id='requests'
+										name='requests'
+										rows='5'
+										className='form-control'
+									/>
+								</Col>
+							</Row>
+							<Row className='form-group'>
+								<Col md={{ size: 10, offset: 2 }}>
+									<Button type='submit' color='primary'>
+										Submit Reservation
+									</Button>
+								</Col>
+							</Row>
+						</Form>
 					</ModalBody>
 				</Modal>
 			</React.Fragment>
 		);
-    }
+	}
 }
 
 export default Header;
