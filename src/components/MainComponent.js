02ef453f10cc43621a60b0bd7693fa2f73137009
Chris Twaddell <ct9233@gmail.com>
2020-10-29 22:14:16 -0700

    Deployment 404 Fix

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -35,7 +35,7 @@ class Main extends Component {
                             <Route exact path='/menu' render={() => <Menu />} />
                             <Route exact path='/story' render={() => <Story />} />
                             <Route exact path='/contact' render={() => <Contact />} />
-                            <Redirect to='/' />
+                            <Redirect to='/home' />
                         </Switch>
                     </CSSTransition>
                 </TransitionGroup>
