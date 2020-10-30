9f5e15f557d1572aff4be03df8a42cedf2be5792
Chris Twaddell <ct9233@gmail.com>
2020-10-29 23:31:15 -0700

    Image Deployment Updates

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -3,25 +3,25 @@ import { UncontrolledCarousel } from 'reactstrap';
 
 const slides = [
 	{
-        src: {`${process.env.PUBLIC_URL}/assets/images/carousel_food.jpg`},
+        src: `${process.env.PUBLIC_URL}/assets/images/carousel_food.jpg`,
         altText: 'Food Image',
         header: 'Food',
         key: '1'
     },
     {
-        src: {`${process.env.PUBLIC_URL}/assets/images/carousel_drink.jpg`},
+        src: `${process.env.PUBLIC_URL}/assets/images/carousel_drink.jpg`,
         altText: 'Drink Image',
         header: 'Drink',
         key: '2'
     },
     {
-        src: {`${process.env.PUBLIC_URL}/assets/images/carousel_dessert.jpg`},
+        src: `${process.env.PUBLIC_URL}/assets/images/carousel_dessert.jpg`,
         altText: 'Dessert Image',
         header: 'Dessert',
         key: '3'
     },
     {
-        src: {`${process.env.PUBLIC_URL}/assets/images/carousel_pizza.jpg`},
+        src: `${process.env.PUBLIC_URL}/assets/images/carousel_pizza.jpg`,
         altText: 'Pizza Image',
         header: 'And PIZZA!!',
         key: '4'
