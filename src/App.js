ac3ebe1cf543d28a2cee7d0cb6dc44ae644e3af1
Chris Twaddell <ct9233@gmail.com>
2020-10-30 00:23:10 -0700

    Deployment Switch Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<HashRouter basename='/'>
 				<div className='App'>
 					<Main />
 				</div>
-			</BrowserRouter>
+			</HashRouter>
 		);
 	}
 }
